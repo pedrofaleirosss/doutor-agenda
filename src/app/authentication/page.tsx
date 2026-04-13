066ed479893ccfbc525e7c8d6646113603887609
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2026-04-13 20:54:55 -0300

    feat: add logo image on authentication page and update its layout

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,4 +1,5 @@
 import { headers } from "next/headers";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -17,7 +18,11 @@ const AuthenticationPage = async () => {
   }
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center">
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-4">
+      <Image src="/logo.svg" alt="Logo" width={400} height={400} />
+      <p className="text-primary w-[400px] text-lg font-medium">
+        Gerencie sua clínica de forma simples e eficiente.
+      </p>
       <Tabs defaultValue="login" className="w-[400px]">
         <TabsList className="grid w-full grid-cols-2">
           <TabsTrigger value="login">Login</TabsTrigger>
